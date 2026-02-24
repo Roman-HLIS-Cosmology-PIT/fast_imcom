Fix calculation of circular cutouts

fast_imcom/io_general.py
@@ -28,12 +28,12 @@ class InSlice:
         self.filename = filename
         self.psfmodel = psfmodel
 
+        self.inx_min = self.iny_min = 0
         if loaddata:
             self.load_data_and_mask()
             if paddata: self.pad_data_and_mask()
         else:
             self.wcs = self.data = self.mask = None
-            self.inx_min = self.iny_min = 0
 
     def load_data_and_mask(self) -> None:
         self.data = np.zeros((InSlice.NLAYER, InSlice.NSIDE, InSlice.NSIDE), dtype=np.float32)
@@ -127,7 +127,7 @@ class OutSlice:
     SAVE_ALL = True  # Whether to save individual regridded images.
 
     @staticmethod
-    def get_outwcs(outcrval: np.ndarray, outcrpix: list[float] = [2044.0, 2044.0],
+    def get_outwcs(outcrval: np.ndarray, outcrpix: list[float, float] = [2044.0, 2044.0],
                    outcdelt: float = 0.11 * u.arcsec.to("degree") / 2.0) -> wcs.WCS:
         outwcs = wcs.WCS(naxis=2)
         outwcs.wcs.ctype = ["RA---STG", "DEC--STG"]
@@ -157,8 +157,7 @@ class OutSlice:
         else:
             self.data = np.zeros((InSlice.NLAYER, self.NPIX_TOT, self.NPIX_TOT), dtype=np.float32)
 
-    def __call__(self, filename: str = None, timing: bool = False,
-                 stop: int = np.inf) -> None:
+    def __call__(self, filename: str = None, timing: bool = False, stop: int = np.inf) -> None:
         if timing: tstart = perf_counter()
         for X in range(self.NSUB):
             if stop > 0 and timing:
@@ -174,9 +173,7 @@ class OutSlice:
         self.mask = np.stack([inslice.mask_out for inslice in self.inslices])
         if not self.SAVE_ALL: self.data /= self.mask.sum(axis=0)
         if InSlice.NLAYER == 1: self.data = self.data[0]
-
-        if filename is not None:
-            self.writeto(filename)
+        if filename is not None: self.writeto(filename)
 
     def writeto(self, filename: str) -> None:
         datahdu = fits.PrimaryHDU(self.data, header=self.wcs.to_header())

fast_imcom/psfutil.py
@@ -4,8 +4,8 @@ PSF utilities in two dimensions.
 
 Functions
 ---------
-psf_gaussian_2d : Generate a 2D Gaussian PSF.
-pixelate_psf_2d : Pixelate a 2D (input) PSF.
+psf_gaussian : Generate a 2D Gaussian PSF.
+pixelate_psf : Pixelate a 2D (input) PSF.
 
 """
 
@@ -20,7 +20,7 @@ class PSFModel:
     NPIX = 48  # PSF array size in native pixels.
     SAMP = 4  # Oversampling rate of PSF arrays.
     NTOT = NPIX * SAMP  # PSF array size in oversampled pixels.
-    YXCTR = NTOT // 2  # PSF array center in oversampled pixels.
+    YXCTR = NTOT / 2  # PSF array center in oversampled pixels.
 
     SIGMA_TO_FWHM = 2 * np.sqrt(2 * np.log(2))
     SIGMA = {
@@ -32,7 +32,7 @@ class PSFModel:
     }
 
     @classmethod
-    def psf_gaussian_2d(cls, sigma: float) -> np.ndarray:
+    def psf_gaussian(cls, sigma: float) -> np.ndarray:
         """Generate a 2D Gaussian PSF."""
         x, y = np.meshgrid(np.arange(-cls.NTOT//2, cls.NTOT//2) / (sigma*cls.SAMP),
                            np.arange(-cls.NTOT//2, cls.NTOT//2) / (sigma*cls.SAMP))
@@ -40,7 +40,7 @@ class PSFModel:
                 / (2.0*np.pi * sigma**2)
 
     @classmethod
-    def pixelate_psf_2d(cls, psf: np.ndarray) -> np.ndarray:
+    def pixelate_psf(cls, psf: np.ndarray) -> np.ndarray:
         """Pixelate a 2D (input) PSF."""
         k = np.linspace(0, 1, cls.NTOT, endpoint=False)
         k[-(cls.NTOT//2):] -= 1; k *= cls.SAMP
@@ -49,15 +49,13 @@ class PSFModel:
 
     @classmethod
     def get_weight_field(cls, psf_in: np.ndarray, psf_out: np.ndarray) -> np.ndarray:
-        psf_inp = cls.pixelate_psf_2d(psf_in)
+        psf_inp = cls.pixelate_psf(psf_in)
         psf_inp_tbl = bandlimited_rfft2(psf_inp[None], cls.NPIX-1)[0]
         psf_out_tbl = bandlimited_rfft2(psf_out[None], cls.NPIX-1)[0]
 
         weight_tbl = psf_out_tbl / psf_inp_tbl; weight_tbl.imag = 0
-        weight = np.fft.ifftshift(bandlimited_irfft2(weight_tbl[None], cls.NTOT, cls.NTOT))[0]
-        weight *= cls.SAMP**2
-
-        return weight
+        return np.fft.ifftshift(bandlimited_irfft2(
+            weight_tbl[None], cls.NTOT, cls.NTOT))[0] * cls.SAMP**2
 
     def __init__(self, psfdata: np.ndarray) -> None:
         self.psfdata = psfdata
@@ -69,8 +67,8 @@ class PSFModel:
 
 class SubSlice:
 
-    ACCEPT = 8  # ACCEPTance radius
-    LOSS_THR = 0.001  # Threshold for total lost weight.
+    ACCEPT = 8  # Acceptance radius in native pixels.
+    LOSS_THR = 0.001  # Threshold for sum of absolute lost weights.
 
     def __init__(self, outslice, X: int, Y: int) -> None:
         self.outslice = outslice
@@ -85,7 +83,7 @@ class SubSlice:
 
         for i_sl, inslice in enumerate(self.outslice.inslices):
             psf_in = inslice.get_psf()
-            psf_out = PSFModel.psf_gaussian_2d(sigma)
+            psf_out = PSFModel.psf_gaussian(sigma)
             weight = PSFModel.get_weight_field(psf_in, psf_out)
 
             inxys = inslice.outpix2world2inpix(self.outslice.wcs, self.outxys)

fast_imcom/routine.py
@@ -128,8 +128,8 @@ def reggridD5512C(infunc: np.array, x0: float, y0: float, SAMP: int,
     for ix in range(ACCEPT2):
         xi = xmin + ix*SAMP
         if circ_cut:
-            cut = ACCEPT - np.int32(np.rint(ACCEPT**2 -\
-                (ix-(ACCEPT-(ix<ACCEPT)))**2) ** 0.5)
+            cut = (ACCEPT-1) - np.int32(((ACCEPT-.5)**2 -\
+                (ix-(ACCEPT-(ix<ACCEPT)))**2) ** .5)
 
         for i in range(cut*SAMP, 10+(ACCEPT2-1)*SAMP):
             interp_vstrip[i] = 0.0